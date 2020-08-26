d0902a932bb87173ff75a03776180eb39379802c
camilafacens <54379899+camilafacens@users.noreply.github.com>
2020-08-26 19:09:02 -0300

    Exercício 1

--- core/views.py
+++ core/views.py
@@ -6,6 +6,5 @@ def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
 
 def soma(request, val1, val2):
-    val1 = 10
-    val2 = 20
+
     return HttpResponse('Soma: {}'.format(val1+val2))
